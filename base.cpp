bb8581f9e6401221afa00427d94566054960de8f
Pedro Ives <47249629+jovemives@users.noreply.github.com>
2019-06-07 14:25:04 -0300

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,24 +1,35 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
 #define pb push_back
-#define eb emplace_back
 #define mk make_pair
 #define fi first
-#define sec second
-#define cc(x)	cout << #x << " = " << x << endl
-#define ok		cout << "ok" << endl
-#define endl '\n'
+#define se second
 
-typedef long long ll;
-typedef pair<int,int> ii;
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+
+#define gnl cout << endl
+#define olar cout << "olar" << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
 const int INF = 0x3f3f3f3f;
-const double PI = acos(-1.0);
- 
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int MOD = 1e9+7;
 
+int main(){
+    fastio;
     
     
+ 
     return 0;
 }
