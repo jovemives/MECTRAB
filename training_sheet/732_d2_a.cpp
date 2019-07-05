f9161e0b1f4e436046bb5be0e306698bda617286
Pedro Ives <47249629+jovemives@users.noreply.github.com>
2019-07-05 20:44:14 -0300

    Add files via upload

---
+++ training_sheet/732_d2_a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+
+#define nl cout << endl
+#define sarve cout << "sarve" << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9+7;
+
+int main(){
+    fastio;
+    
+	const int N = 1e3 + 10;
+	int k, r, ans = 0;
+	cin >>k >>r;
+ 
+	for(int i = 1; i < 10; i++){
+		if((i*k)%10 == 0 or (i*k)%10 == r){
+			cout <<i <<endl;
+			return 0;
+		}
+	}
+    return 0;
+}
+
